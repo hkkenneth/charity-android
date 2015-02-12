a2d05e90b7b20fffcbb6a7191f4e1e8c9b8980b3
Ryan Leung <rwcleung@gmail.com>
2015-02-12 16:51:39 +0800

    Remove unnecessary import

--- CharityNow/app/src/main/java/com/winginno/charitynow/Model/ScheduledTasks/BootReceiver.java
+++ CharityNow/app/src/main/java/com/winginno/charitynow/Model/ScheduledTasks/BootReceiver.java
@@ -1,14 +1,10 @@
 package com.winginno.charitynow.Model.ScheduledTasks;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import com.winginno.charitynow.DataFetchTask;
-import com.winginno.charitynow.GcmIntentService;
-import com.winginno.charitynow.R;
 
 public class BootReceiver extends BroadcastReceiver {
 
